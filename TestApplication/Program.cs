1e5c0de4bcab0925f37ba3ae7d2f619319751387
Arjun Haridas <arjunharidas@carestack.com>
2025-07-11 18:39:39 +0530

    Adding files

---
+++ TestApplication/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+
+namespace TestApplication;
+
+public class Program
+{
+    public static void Main()
+    {
+        var person = new Person() { Id = 1, Name = "John" };
+
+        var sw = Stopwatch.StartNew();
+        for (int i = 0; i < 10000; i++)
+        {
+            person.MapToEmployee();
+        }
+        sw.Stop();
+        Console.WriteLine($"Time ellapsed for quick mapping - {sw.Elapsed.TotalMilliseconds} ms");
+
+        sw = Stopwatch.StartNew();
+        for (int i = 0; i < 10000; i++)
+        {
+            var employee = SlowMapper.SlowMapper.Map<Person, Employee>(person);
+        }
+        sw.Stop();
+        Console.WriteLine($"Time ellapsed for slow mapping - {sw.Elapsed.TotalMilliseconds} ms");
+    }
+}
